1cedb4e4b309dffeef4e3516a44050b135cc7c7c
Carolina Reedy <carinvid@gmail.com>
2021-09-19 16:30:30 -0600

    fisrt commit

--- lets-party/src/App.js
+++ lets-party/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
+import Nav from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "starts",
+              stroke: {
+                width: 6,
+                color: "#ea33ff",
+              },
+            },
+          },
+        }}
+      />
+      <Nav />
+      <Header />
+    </>
   );
 }
 

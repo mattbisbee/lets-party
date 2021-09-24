69d481d08ab52ead79f2a2568f3034628044e0a7
Matt Bisbee <82290275+mattbisbee@users.noreply.github.com>
2021-09-23 20:07:24 -0700

    added gitignore file to server folder

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,9 @@
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
+const stripe = require('stripe')('process.env.STRIPE_KEY');
+
+require('dotenv').config();
 
 const resolvers = {
     Query: {
@@ -51,42 +54,48 @@ const resolvers = {
 
             throw new AuthenticationError('Not logged in');
         },
+        
+        //checkout method
+        checkout: async (parent, args, context) => {
+            const url = new URL(context.headers.referer).origin;
+            //Using 'Order' Mongoose Model to covert these ids into populated product objects
+            const order = new Order({ products: args.products });
+            const { products } = await order.populate('products').execPopulate();
 
-        // checkout: async (parent, args, context) => {
-        //     const url = new URL(context.headers.referer).origin;
-        //     const order = new Order({ products: args.products });
-        //     const { products } = await order.populate('products').execPopulate();
-        //     const line_items = [];
+            // use Stripe API to create a product ID and a price ID for each item. Loops over products from the 'Order' model and pushes price ID for each one into a new 'line_items" array
+            const line_items = [];
 
-        //     for (let i = 0; i < products.length; i++) {
-        //         const product = await stripe.products.create({
-        //             name: products[i].name,
-        //             description: products[i].description,
-        //             images: [`${url}/images/$${products[i].image}`]
-        //         });
+            for (let i = 0; i < products.length; i++) {
+                // product id
+                const product = await stripe.products.create({
+                    name: products[i].name,
+                    description: products[i].description,
+                    images: [`${url}/images/$${products[i].image}`]
+                });
+                // price id
+                const price = await stripe.prices.create({
+                    product: product.id,
+                    //convert cents to dollars by mulitplying by 100
+                    unit_amount: products[i].price * 100,
+                    currency: 'usd'
+                });
+                // add price id to line items array
+                line_items.push({
+                    price: price.id,
+                    quantity: 1
+                });
+            }
 
-        //         const price = await stripe.prices.create({
-        //             product: product.id,
-        //             unit_amount: products[i].price * 100,
-        //             currency: 'usd'
-        //         });
+            const session = await stripe.checkout.sessions.create({
+                payment_method_types: ['card'],
+                line_items,
+                mode: 'payment',
+                success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${url}`
+            });
 
-        //         line_items.push({
-        //             price: price.id,
-        //             quantity: 1
-        //         });
-        //     }
-
-        //     const session = await stripe.checkout.sessions.create({
-        //         payment_method_types: ['card'],
-        //         line_items,
-        //         mode: 'payment',
-        //         success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        //         cancel_url: `${url}`
-        //     });
-
-        //     return { session: session.id }
-        // }
+            return { session: session.id }
+        }
     },
     Mutation: {
         addUser: async (parent, args) => {

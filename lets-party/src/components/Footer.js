07694f551225bb00710db1d84b68ecd8f2389080
Matt Bisbee <82290275+mattbisbee@users.noreply.github.com>
2021-09-27 20:54:36 -0700

    added icons and manifest.json

--- lets-party/src/components/Footer.js
+++ lets-party/src/components/Footer.js
@@ -63,6 +63,7 @@ const Footer = () => {
           Copyright&copy; {new Date().getFullYear()}&nbsp; In One Basket | All
           Rights Reserved
         </p>
+        <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       </div>
     </div>
   );

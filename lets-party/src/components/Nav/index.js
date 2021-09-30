53fe5ad49088dd2551c8d56ed04241cbe6378fa0
Carolina Reedy <carinvid@gmail.com>
2021-09-29 23:26:04 -0600

    fix error

--- lets-party/src/components/Nav/index.js
+++ lets-party/src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 
-import '../../index.css'
+import "../../index.css";
 import "../../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,58 +10,39 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Basket3Fill } from "react-bootstrap-icons";
 import { PersonFill } from "react-bootstrap-icons";
 import { HouseFill } from "react-bootstrap-icons";
-import { ChatTextFill } from 'react-bootstrap-icons';
+import { ChatTextFill } from "react-bootstrap-icons";
 
 const linkStyle = {
   margin: "5rem",
-  textDecoration: "none"
+  textDecoration: "none",
 };
 
 function Nav() {
   function displayNav() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="navbar-nav m-auto">
-          <li className="nav-item">
-            <a href="/" onClick={() => Auth.logout()}>
-              Logout
-            </a>
-          </li>
-        </ul>
-      );
-    } else {
-      return (
-          <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-            <div className="container">
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <FontAwesomeIcon icon={faBars} style={{ color: "white" }} />
-              </button>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+          <div className="container">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <FontAwesomeIcon icon={faBars} style={{ color: "white" }} />
+            </button>
 
-            <div className="collapse navbar-collapse m-auto" id="navbarSupportedContent">
+            <div
+              className="collapse navbar-collapse m-auto"
+              id="navbarSupportedContent"
+            >
               <ul className="navbar-nav m-auto">
                 <li>
                   <Link to="/" style={linkStyle}>
-                   <HouseFill /> Home
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/signup" style={linkStyle}>
-                    <PersonFill /> Signup
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/login" style={linkStyle}>
-                  <PersonFill /> Login
+                    <HouseFill /> Home
                   </Link>
                 </li>
 
@@ -73,7 +54,67 @@ function Nav() {
 
                 <li>
                   <Link to="/basket" style={linkStyle}>
-                   <Basket3Fill /> Basket
+                    <Basket3Fill /> Basket
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <a href="/" onClick={() => Auth.logout()}>
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      );
+    } else {
+      return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+          <div className="container">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <FontAwesomeIcon icon={faBars} style={{ color: "white" }} />
+            </button>
+
+            <div
+              className="collapse navbar-collapse m-auto"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav m-auto">
+                <li>
+                  <Link to="/" style={linkStyle}>
+                    <HouseFill /> Home
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="/signup" style={linkStyle}>
+                    <PersonFill /> Signup
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="/login" style={linkStyle}>
+                    <PersonFill /> Login
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="/contact" style={linkStyle}>
+                    <ChatTextFill /> Contact
+                  </Link>
+                </li>
+
+                <li>
+                  <Link to="/basket" style={linkStyle}>
+                    <Basket3Fill /> Basket
                   </Link>
                 </li>
               </ul>
@@ -86,9 +127,7 @@ function Nav() {
 
   return (
     <header className="flex-row px-1">
-      <nav>
-        {displayNav()}
-      </nav>
+      <nav>{displayNav()}</nav>
     </header>
   );
 }
